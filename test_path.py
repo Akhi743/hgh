f2d64dca3b1a30def816a8e89c025cd0e73c17e7
Akhi743 <82310742+Akhi743@users.noreply.github.com>
2025-06-05 17:26:30 -0400

    Delete test_path.py

--- test_path.py
+++
@@ -1,4 +0,0 @@
-import os
-print("Current working directory:", os.getcwd())
-print("Does Dataset/lalonde.csv exist?", os.path.exists("Dataset/lalonde.csv"))
-print("Full path:", os.path.abspath("Dataset/lalonde.csv"))
